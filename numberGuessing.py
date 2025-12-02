456428c1abb67dd5a971d39678ca37c9a2ff5862
Jakob Neri <jakobneri@users.noreply.github.com>
2025-12-02 08:37:36 +0100

    Refactor game loop to use a boolean flag for better control

--- numberGuessing.py
+++ numberGuessing.py
@@ -1,11 +1,12 @@
 import random
 secret = random.randint(1, 100)
 attempts = 0
+game=True
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
-while True:
+while game:
     guess = int(input("Make a guess: "))
     if guess < secret:
         attempts += 1
@@ -16,7 +17,9 @@ while True:
     elif guess == secret:
         attempts += 1
         print(f"Congratulations! You've guessed the number {secret} in {attempts} attempts.")
+        game=False
         break
     else:
         print("Invalid input. Please enter a number between 1 and 100.")
+
 
