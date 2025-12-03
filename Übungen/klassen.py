da8d1acc48e38381449dee5d145a3fc2bf680298
Jakob Neri <jakobneri@users.noreply.github.com>
2025-12-03 12:39:50 +0100

    Statischer Member

--- Übungen/klassen.py
+++ Übungen/klassen.py
@@ -1,10 +1,13 @@
 class car():
+    carCounter = 0
+
     def __init__(self, hersteller, leistung, farbe):
         self.hersteller = hersteller
         self.leistung = leistung
         self.farbe = farbe
         self.Xpos = 5
         self.Ypos = 5
+        car.carCounter += 1
 
     def move(self, x, y):
         self.Xpos += x
@@ -20,3 +23,4 @@ print(f"Auto 1: {car1.hersteller}, Leistung: {car1.leistung} PS, Farbe: {car1.fa
 print(f"Auto 2: {car2.hersteller}, Leistung: {car2.leistung} PS, Farbe: {car2.farbe}")
 
 print(f"Auto 1 Position: {car1.position()}")
+print(f"Es gibt insgesamt {car.carCounter} Autos.")
